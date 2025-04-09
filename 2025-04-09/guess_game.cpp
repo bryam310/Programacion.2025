2e8d35f02e9440d23eab67225d71c2f35d36a9bf
Bryam Felipe <bbastidasz@.unal.edu.co>
2025-04-09 08:35:53 -0500

    agregue el guess_game.cpp

--- 2025-04-09/guess_game.cpp
+++ 2025-04-09/guess_game.cpp
@@ -13,16 +13,20 @@ Informae si lo adivino
 int main(void) {
     const int NUM = 10;
     int guessed_number;
-    std::cout << "adivina un numero entre 1 y 100:\n";
-    std::cin >> guessed_number;
 
-if (guessed_number == NUM) {
-    std::cout << "GANASTE!!!!\n";
-}else if (guessed_number > NUM){
-    std::cout << "te pasaste.\n";
-} else { // menor
-    std::cout << "te falta.\n";
-}
+   for ( ; ; ) {        
+        std::cout << "adivina un numero entre 1 y 100:\n";
+        std::cin >> guessed_number;
+
+        if (guessed_number == NUM) {
+            std::cout << "GANASTE!!!!\n";
+            break;
+        }else if (guessed_number > NUM){
+            std::cout << "te pasaste.\n";
+        } else { // menor
+            std::cout << "te falta.\n";
+        }
+    }
 
  return 0;
 }
