e7f1f37af268fb78b86604085dfa57b3a6fccd09
Bryam Felipe <bbastidasz@.unal.edu.co>
2025-04-09 07:30:18 -0500

    subi guess_game.cpp

---
+++ 2025-04-09/guess_game.cpp
@@ -0,0 +1,20 @@
+/*
+juego de adivinar numero 
+
+tener un numero 
+preguntar al usuario el numero 
+Informae si lo adivino
+
+*/
+
+
+#include <iostream>
+
+int main(void) {
+    const int NUM = 10;
+    int guessed_number;
+    std::cout << "adivina un numero entre 1 y 100:\n";
+    std::cin >> guessed_number;
+
+    return 0;
+}
